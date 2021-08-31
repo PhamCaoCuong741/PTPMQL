596f64bcd21dca11d547035c3c48c6a9e78ed74c
CUONG PHAM CAO <1821050741@student.humg.edu.vn>
2021-08-31 10:40:09 +0700

    Add project files.

---
+++ PTPMQL/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTPMQL.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult GPT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPT(string soX, string soY)
+        {
+            double soa = Convert.ToDouble(soX);
+            double sob = Convert.ToDouble(soY);
+            double ketqua = -sob / soa;
+            ViewBag.Giaipt = ketqua;
+            return View();
+        }
+    }
+}
---
+++ PTPMQL/Models/GPTB1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTPMQL.Models
+{
+    public class GPTB1
+    {
+        public double GPT(double x, double y)
+        {
+            double GPT = 0;
+            GPT = -x / y;
+            return GPT;
+        }
+    }
+}
